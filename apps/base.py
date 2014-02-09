3f5244cdf6c1731edda67b4ede156e9804fe595e
paulocheque <paulocheque@gmail.com>
2014-02-09 21:10:59 -0200

    alert component

--- apps/base.py
+++ apps/base.py
@@ -18,6 +18,8 @@ class BaseHandler(tornado.web.RequestHandler):
     def render(self, template_name, **kwargs):
         if 'alert' not in kwargs:
             kwargs['alert'] = None
+        if 'alert_type' not in kwargs:
+            kwargs['alert_type'] = None
         if 'current_user' not in kwargs:
             kwargs['current_user'] = self.get_current_user()
         return super(BaseHandler, self).render(template_name, **kwargs)
