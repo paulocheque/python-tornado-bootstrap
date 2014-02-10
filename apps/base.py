9dd10f51c0bb73543d9d715c26d1e8f3ff00931c
paulocheque <paulocheque@gmail.com>
2014-02-10 12:09:16 -0200

    CachedBaseHandler

--- apps/base.py
+++ apps/base.py
@@ -2,6 +2,9 @@
 import tornado.web
 
 from apps.accounts.models import User
+import connect_redis
+
+redis_connection = connect_redis.connect_to_redis()
 
 
 class BaseHandler(tornado.web.RequestHandler):
@@ -23,3 +26,18 @@ class BaseHandler(tornado.web.RequestHandler):
         if 'current_user' not in kwargs:
             kwargs['current_user'] = self.get_current_user()
         return super(BaseHandler, self).render(template_name, **kwargs)
+
+
+class CachedBaseHandler(BaseHandler):
+    def prepare(self):
+        cached = redis_connection.get(self.request.uri)
+        if cached is not None:
+            # print('Read cached page for %s' % self.request.uri)
+            self.write(cached)
+            self.finish()
+
+    def render_string(self, template_name, **kwargs):
+        html_generated = super(CacheBaseHandler, self).render_string(template_name, **kwargs)
+        redis_connection.set(self.request.uri, html_generated)
+        # print('Page %s cached' % self.request.uri)
+        return html_generated
