9375307d54941be45431fcc51b83fb72e339eb40
paulocheque <paulocheque@gmail.com>
2014-02-26 09:03:51 +0100

    User.admin property

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -9,10 +9,45 @@ from mongoengine import *
 
 class User(Document):
     email = EmailField(required=True)
-    password = StringField(required=True)
+    password = StringField() # social login
     secret_key = StringField(required=True)
+    admin = BooleanField()
 
     registered_on = DateTimeField(required=True, default=datetime.utcnow)
+
+    @classmethod
+    def authenticate(cls, email, password):
+        if password:
+            pw = User.encrypt_password(password)
+            return User.objects(email=email, password=pw)
+        return []
+
+    @classmethod
+    def encrypt_password(cls, password):
+        return sha.sha(password).hexdigest()
+
+    @classmethod
+    def generate_secret_key(cls):
+        return os.urandom(24).encode('base64').strip()
+
+    @classmethod
+    def is_valid_password(cls, password):
+        if len(password) < 10 or len(password) > 512: return False
+        if re.match('.*\s+', password): return False
+        if not re.match('.*[a-z]+', password): return False
+        if not re.match('.*[A-Z]+', password): return False
+        if not re.match('.*[0-9]+', password): return False
+        if not re.match('.*[!@#$%&*()_+-={}|/?;:,.<>\\\[\]]+', password): return False
+        return True
+
+    def save(self, encrypt_pass=False, **kwargs):
+        if encrypt_pass:
+            self.validate_password()
+            self.password = User.encrypt_password(self.password)
+        created = self.id is None
+        if not self.secret_key:
+            self.secret_key = User.generate_secret_key()
+        super(User, self).save(**kwargs)
 
     def validate_password(self):
         errors = {}
@@ -27,25 +62,6 @@ class User(Document):
         if errors:
             raise ValidationError('ValidationError', errors=errors)
 
-    def save(self, encrypt_pass=False, **kwargs):
-        if encrypt_pass:
-            self.validate_password()
-            self.password = User.encrypt_password(self.password)
-        created = self.id is None
-        if not self.secret_key:
-            self.generate_secret_key()
-        super(User, self).save(**kwargs)
-
-    def generate_secret_key(self):
-        self.secret_key = os.urandom(24).encode('base64').strip()
-
-    @classmethod
-    def authenticate(cls, email, password):
-        if password:
-            pw = User.encrypt_password(password)
-            return User.objects(email=email, password=pw)
-        return []
-
     def change_password(self, current_password, new_password):
         errors = {}
         if User.encrypt_password(current_password) != self.password:
@@ -57,21 +73,8 @@ class User(Document):
         self.password = new_password
         self.save(encrypt_pass=True)
 
-    @classmethod
-    def encrypt_password(cls, password):
-        return sha.sha(password).hexdigest()
-
-    @classmethod
-    def is_valid_password(cls, password):
-        if len(password) < 10 or len(password) > 512: return False
-        if re.match('.*\s+', password): return False
-        if not re.match('.*[a-z]+', password): return False
-        if not re.match('.*[A-Z]+', password): return False
-        if not re.match('.*[0-9]+', password): return False
-        if not re.match('.*[!@#$%&*()_+-={}|/?;:,.<>\\\[\]]+', password): return False
-        return True
 
 # Migration
-users = User.objects.filter(secret_key=None)
-for u in users:
-    u.save()
+# users = User.objects.filter(secret_key=None)
+# for u in users:
+#     u.save()
