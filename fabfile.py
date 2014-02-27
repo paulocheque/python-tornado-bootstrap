bb82d94e5b2cf2d70834ee07b609eb487300b475
paulocheque <paulocheque@gmail.com>
2014-02-27 08:25:43 +0100

    colorize for fabfile

--- fabfile.py
+++ fabfile.py
@@ -12,6 +12,15 @@ from apps.accounts.models import User
 from apps.app.tasks import *
 
 
+# https://gist.github.com/paulocheque/5906909
+def colorize(message, color='blue'):
+    color_codes = dict(black=30, red=31, green=32, yellow=33, blue=34, magenta=35, cyan=36, white=37)
+    code = color_codes.get(color, 34)
+    msg = '\033[%(code)sm%(message)s\033[0m' % {'code':code, 'message':message}
+    # print(msg)
+    return msg
+
+
 @task
 def task1():
     a_task()
