75778c05fac24086514f994e9cbd417cefb8ad09
paulocheque <paulocheque@gmail.com>
2014-03-02 11:37:37 +0100

    Base footer. Base social icons. main.html in app. Social buttons in main.html

--- apps/app/handlers.py
+++ apps/app/handlers.py
@@ -7,6 +7,6 @@ from apps.utils.base import BaseHandler
 class HomeHandler(BaseHandler):
     def get(self):
         if self.get_current_user():
-            self.render('main.html')
+            self.render('app/main.html')
         else:
-            self.render('main.html', alert='You have to login first', alert_type='alert-info')
+            self.render('app/main.html', alert='You have to login first', alert_type='alert-info')
