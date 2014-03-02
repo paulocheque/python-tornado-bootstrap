5971a094d5d34f56d04692900efd476e51eef283
paulocheque <paulocheque@gmail.com>
2014-03-02 21:38:54 +0100

    Fixed bad variables in utls.tasks module

--- apps/utils/tasks.py
+++ apps/utils/tasks.py
@@ -2,6 +2,7 @@ import logging
 import os
 import re
 from StringIO import StringIO
+import sys
 
 import requests
 import sendgrid
@@ -13,17 +14,16 @@ def get_lat_long_from_ip(ip):
         r = r.json()
         return (float(r['latitude']), float(r['longitude']))
     except KeyError as e:
-        print(ip, r)
-        raise e
+        raise e, None, sys.exc_info()[2]
 
 
 def send_admin_mail(subject, body):
     try:
         sg = sendgrid.SendGridClient(os.getenv('SENDGRID_USERNAME'), os.getenv('SENDGRID_PASSWORD'))
         message = sendgrid.Mail(to='paulocheque@gmail.com',
-            subject=msg,
-            html=msg,
-            text=msg,
+            subject=subject,
+            html=body,
+            text=body,
             from_email='contact@paulocheque.com')
         sg.send(message)
     except Exception as e:
