4aa66b9408d9d182553dcc4b8d38a8f85a4dfe68
paulocheque <paulocheque@gmail.com>
2014-03-04 15:16:41 +0100

    add_contact_to_propagation task

--- apps/utils/tasks.py
+++ apps/utils/tasks.py
@@ -29,3 +29,29 @@ def send_admin_mail(subject, body):
     except Exception as e:
         logging.error(str(e))
         logging.exception(e)
+
+
+def add_contact_to_propagation(email=None, fb_id=None, tags=None, languages=None, debug=False):
+    if email or fb_id:
+        if debug:
+            domain = 'http://localhost:5000'
+        else:
+            domain = 'http://propagation.herokuapp.com'
+        try:
+            payload = {}
+            if email:
+                payload['email'] = email
+            if fb_id:
+                payload['fb_id'] = fb_id
+            if tags:
+                if isinstance(tags, (list, set)):
+                    tags = ','.join(tags)
+                payload['tags'] = tags
+            if languages:
+                if isinstance(languages, (list, set)):
+                    languages = ','.join(languages)
+                payload['languages'] = languages
+            r = requests.post('%s/api/create-update-contact' % domain, data=payload)
+            print('[%s] http POST /create-update-contact' % (r.status_code,))
+        except KeyError as e:
+            logging.exception(e)
