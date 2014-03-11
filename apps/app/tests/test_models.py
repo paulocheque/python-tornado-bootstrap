5d96b4f00fbd85ee4b1a32ebf80ec3828d7d9b18
paulocheque <paulocheque@gmail.com>
2014-03-11 09:00:34 +0100

    more test example

--- apps/app/tests/test_models.py
+++ apps/app/tests/test_models.py
@@ -1,8 +1,7 @@
 # coding: utf-8
 from datetime import datetime, timedelta
 import unittest
-
-#from nose.tools import raises
+from nose.tools import raises
 
 from apps.utils.tests.base import MongoEngineTestCase
 from ..models import *
@@ -14,7 +13,13 @@ class AlgorithmTests(unittest.TestCase):
 
 
 class MyDocTests(MongoEngineTestCase):
+    # @raises(Exception)
     def test_(self):
         self.assertEquals(True, True)
+
+    @raises(Exception)
+    def test_raises(self):
+        raise Exception()
+
 
 
