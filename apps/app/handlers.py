556743fcbf5d349c4284886592c9eee53d4f8899
paulocheque <paulocheque@gmail.com>
2014-03-21 11:10:28 +0100

    Splitting base templates to avoid replicated code

--- apps/app/handlers.py
+++ apps/app/handlers.py
@@ -6,7 +6,15 @@ from apps.utils.base import BaseHandler
 
 class HomeHandler(BaseHandler):
     def get(self):
-        if self.get_current_user():
-            self.render('app/main.html')
+        style = self.get_argument('style', 'default')
+        if style == 'print':
+            template = 'app/main_print.html'
+        elif style == 'noads':
+            template = 'app/main_no_ads.html'
         else:
-            self.render('app/main.html', alert='You have to login first', alert_type='alert-info')
+            template = 'app/main.html'
+
+        if self.get_current_user():
+            self.render(template)
+        else:
+            self.render(template, alert='You have to login first', alert_type='alert-info')
