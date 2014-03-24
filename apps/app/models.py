e5f598569451df33fba632cfc204abde81e4b31f
paulocheque <paulocheque@gmail.com>
2014-03-24 16:41:07 +0100

    More useful requirements and good model template

--- apps/app/models.py
+++ apps/app/models.py
@@ -1,11 +1,32 @@
 # coding: utf-8
 from datetime import datetime, timedelta
+from decimal import Decimal, ROUND_HALF_UP
+import itertools
+import logging
+import os
+import re
+from StringIO import StringIO
+import urllib
+
+# import qrcode
+
+from apps.utils.serializer import *
+from apps.utils.common import *
+from apps.utils.security import *
+from apps.utils.tasks import *
+from apps.accounts.models import User
 
 from mongoengine import *
 
-from apps.utils.common import *
-
 import connect_redis
+
+
+SYSTEM_NAME = os.getenv('SYSTEM_NAME')
+SYSTEM_EMAIL = os.getenv('SYSTEM_EMAIL')
+SYSTEM_URL = os.getenv('SYSTEM_URL')
+DATE_FORMAT = os.getenv('DATE_FORMAT')
+TIME_FORMAT = os.getenv('TIME_FORMAT')
+DATETIME_FORMAT = os.getenv('DATETIME_FORMAT')
 
 
 class MyDoc(Document):
